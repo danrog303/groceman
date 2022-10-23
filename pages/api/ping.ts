0cc8ca26636ddff32eacabba2c845670c7025af7
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-10-23 10:28:54 +0200

    ⚗ Add example database connection to /api/ping endpoint

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import oracledb from "oracledb";
 
 type PingResponse = {
   pong: boolean
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<PingResponse>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<PingResponse>) {
+  const credentials = { user: "groceman_user", password: "groceman_password", connectionString: "localhost:1521/groceman" };
+  const connection = await oracledb.getConnection(credentials);
   res.status(200).json({ pong: true })
 }
